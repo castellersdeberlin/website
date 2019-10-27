ddca080975f8e8542e477047d228369f2ef1fcaf
Laura Luiz <laura.luiz@commercetools.de>
2019-10-27 20:10:25 +0100

    Give calendar 100% width

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -1,6 +1,17 @@
 <template>
-  <div>
-    <iframe src="https://calendar.google.com/calendar/embed?src=q1nclm4a098jqq7od7i11u8so8%40group.calendar.google.com&ctz=Europe%2FBerlin" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>  </div>
+  <v-content>
+    <v-row>
+      <v-col>
+        <iframe src="https://calendar.google.com/calendar/embed?src=q1nclm4a098jqq7od7i11u8so8%40group.calendar.google.com&ctz=Europe%2FBerlin"
+                style="border: 0"
+                width="100%"
+                height="600"
+                frameborder="0"
+                scrolling="no">
+        </iframe>
+      </v-col>
+    </v-row>
+  </v-content>
 </template>
 
 <script>
