00cf448d485fd96747bf2cf5f7a536a991418e5f
Laura Luiz <laura.luiz@commercetools.de>
2019-11-09 13:57:14 +0100

    Improve nav menu structure

---
+++ src/components/LanguageSelector.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-menu>
+    <template v-slot:activator="{ on }">
+      <v-btn v-on="on"
+             depressed
+             class="text-uppercase font-weight-black theme--light v-subheader v-btn--flat">
+        <v-icon>language</v-icon>
+        &nbsp; {{ $i18n.locale }}
+      </v-btn>
+    </template>
+    <v-list>
+      <v-list-item v-for="({ text, value }) in languages"
+                   :key="value"
+                   @click="changeLanguage(value)">
+        <v-list-item-title class="overline">{{ text }}</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-menu>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    languages: [
+      { text: 'Deutsch', value: 'de' },
+      { text: 'English', value: 'en' },
+      { text: 'Català', value: 'ca' },
+    ],
+  }),
+  methods: {
+    changeLanguage(lang) {
+      this.$root.$i18n.locale = lang;
+    },
+  },
+};
+</script>
