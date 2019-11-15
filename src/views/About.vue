4b9c8863626ff4007dd313d42e87fdd9567f51af
Laura Luiz <laura.luiz@commercetools.de>
2019-11-15 10:42:16 +0100

    Fix text history

--- src/views/About.vue
+++ src/views/About.vue
@@ -64,15 +64,16 @@ export default {
 en:
   ourHistory: Our history
   organization: Organization chart
-  about1: "Castellers de Berlin is a group founded in January 2018 by people who wanted to bring
+  about1: "Castellers de Berlin is a group founded in 2018 by people who wanted to bring
     a piece of this century-long Catalan tradition to the German Capital. There have been other
     groups of Castellers in Germany but this is the first and only group in Berlin."
   about2: "The group is established as an apolitical multicultural organisation, welcoming people
-    from all over the world. The goal is to promote the tradition of \"castells\" in Berlin, so far
-    by rehearsing and making public performances in the city."
-  about3: "Castellers de Berlin have regularly been training since April and have shown a strong
-    will to rehearse as it is no easy task to build castells. Put shortly: building castells
-    can only be achieved with teamwork, trust in each other and the will to be better every day."
+    from all over the world. The goal is to promote the tradition of \"castells\" in Berlin
+    by making public performances in the city."
+  about3: "Castellers de Berlin have been training regularly since its foundation and have shown
+    a strong will to rehearse, as it is no easy task to build castells. Put shortly: building
+    castells can only be achieved with teamwork, trust in each other and the will to be better
+    every day."
 de:
   ourHistory: Unsere Geschichte
   organization: Organigramm
@@ -82,21 +83,21 @@ de:
   about2: "Die Gruppe ist eine unpolitische multikulturelle Organisation, in der Menschen aus der
     ganzen Welt willkommen sind. Das Ziel ist, die \"Castell\" Tradition in Berlin zu fördern: fürs
     Erste durch Proben und durch öffentliche Aufführungen."
-  about3: "Castellers de Berlin haben seit April regelmäßig trainiert, und haben einen starken
-    Lernwillen gezeigt, da es keine einfache Aufgabe ist, Castells zu bauen. Kurz gesagt: das Bauen
-    von Castells wird durch Teamarbeit, Vertrauen und den Willen, jeden Tag besser zu werden,
-    erreicht!"
+  about3: "Castellers de Berlin haben seit der Gründung regelmäßig trainiert, und haben einen
+    starken Lernwillen gezeigt, da es keine einfache Aufgabe ist, Castells zu bauen. Kurz gesagt:
+    das Bauen von Castells wird durch Teamarbeit, Vertrauen und den Willen, jeden Tag besser zu
+    werden, erreicht!"
 ca:
   ourHistory: La nostra història
   organization: Organigrama
-  about1: "Al gener de 2018 un grup de gent que volia portar la tradició castellera a la capital
-    alemanya va fundar els castellers de Berlín. Hi ha hagut altres colles castelleres a Alemanya,
-    però aquesta és la primera i única colla castellera a Berlín."
+  about1: "Al 2018 un grup de gent que volia portar la tradició castellera a la capital alemanya va
+    fundar els Castellers de Berlín. Hi ha hagut altres colles castelleres a Alemanya, però aquesta
+    és la primera i única colla castellera a Berlín."
   about2: "Els grup s'ha creat com una organització multicultural que acull a gent d'arreu i està
     lliure de lligams polítics. L'objectiu és promoure el fet casteller a Berlín tot organitzant
-    assaigs i actuacions públiques."
-  about3: "Els Castellers de Berlín assagen de forma regular des d’abril del 2018 i ja s'ha
-    demostrat la voluntat d'assajar fort per fer castells. De forma breu, fer castells
+    actuacions públiques."
+  about3: "Els Castellers de Berlín assagen de forma regular des de la fundació de la colla, i ja
+    s'ha demostrat la voluntat d'assajar fort per fer castells. De forma breu, fer castells
     s'aconsegueix amb bon treball d'equip, confiança dels uns amb els altres i la voluntat de
     ser millors cada dia."
 </i18n>
