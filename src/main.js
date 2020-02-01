ffcef2ea314dd5ae46aa01e206fee46c88285e99
Laura Luiz <laura.luiz@commercetools.de>
2020-02-01 21:23:32 +0100

    Implement basic media gallery by @rotegras

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import VueYouTubeEmbed from 'vue-youtube-embed';
 import vuetify from './plugins/vuetify';
 import App from './App.vue';
 import router from './router';
@@ -7,6 +8,7 @@ import i18n from './i18n';
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
+Vue.use(VueYouTubeEmbed);
 
 new Vue({
   vuetify,
