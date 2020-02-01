6871365dd7f37782d35613c8cd987d2930b72bbb
Laura Luiz <laura.luiz@commercetools.de>
2020-02-01 17:06:23 +0100

    Use browsers language to select

--- src/i18n.js
+++ src/i18n.js
@@ -3,8 +3,12 @@ import VueI18n from 'vue-i18n';
 
 Vue.use(VueI18n);
 
+function findLocale() {
+  return localStorage.getItem('lang') || navigator.language.split('-')[0];
+}
+
 const i18n = new VueI18n({
-  locale: localStorage.getItem('lang') || 'en',
+  locale: findLocale(),
   fallbackLocale: 'en',
 });
 
