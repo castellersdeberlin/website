bffae3f9adce9a37ec27e880fffb3212505a99eb
sailorsilv <81513020+sailorsilv@users.noreply.github.com>
2021-05-04 20:44:36 +0200

    Update NotFound.vue

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -8,13 +8,7 @@
           <v-card height="400" color="transparent" flat>
             <div class="display-3 mt-5">Page not found.</div>
             <div class="grey--text lighten-5">
-              The page you are trying to get to never existed in this reality,
-              or has migrated to a parallel universe.
-            </div>
-            <div class="paragraph-text mt-2">
-              <br />
-              <br />Try going back to home page and repeating your action. Or,
-              contact helpdesk for support.
+              The page you are trying to get does not exist.
             </div>
           </v-card>
         </v-flex>
